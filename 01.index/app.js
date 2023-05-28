04932f46b21370858b4c528cf9f97872dd90dbb7
Mateas Petru <mateaspetru@gmail.com>
2023-05-28 17:42:43 +0300

    addEventListener on detail button

--- 01.index/app.js
+++ 01.index/app.js
@@ -1,8 +1,8 @@
-fetch("https://646e69389c677e23218ba227.mockapi.io/Products")
-  .then((result) => result.json())
-  .then(
-    (products) =>
-      (document.querySelector(".content").innerHTML = products
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("https://646e69389c677e23218ba227.mockapi.io/Products")
+    .then((result) => result.json())
+    .then((products) => {
+      document.querySelector(".content").innerHTML = products
         .map(
           (product) =>
             `<div class="card" style="width: 18rem;">
@@ -11,11 +11,24 @@ fetch("https://646e69389c677e23218ba227.mockapi.io/Products")
                   <h5 class="card-title">${product.productName}</h5>
                   <span>
                     <p class="card-text">${product.price} RON</p>
-                    <a href="#" data-product-id="${product.id}" class="btn btn-primary">Detail</a>
+                    <a data-product-id="${product.id}" class="btn btn-primary detailButton">Detail</a>
                   </span>
-               </div>   
+               </div>
             </div>
         </div>`
         )
-        .join(""))
-  );
+        .join("");
+
+      // crearea paginii detail
+
+      let detailButtons = document.querySelectorAll(".detailButton");
+      console.log(detailButtons);
+      detailButtons.forEach((detailButton) => {
+        detailButton.addEventListener("click", function () {
+          const productId = detailButton.getAttribute("data-product-id");
+
+          console.log(`ai dat click pe produsul cu ID ${productId}`);
+        });
+      });
+    });
+});
