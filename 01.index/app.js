4535262938702f295211de3d5d9dc8be2e3b4171
Mateas Petru <mateaspetru@gmail.com>
2023-05-29 06:46:13 +0300

    open detail page on click and style for detail page

--- 01.index/app.js
+++ 01.index/app.js
@@ -1,11 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("https://646e69389c677e23218ba227.mockapi.io/Products")
-    .then((result) => result.json())
-    .then((products) => {
-      document.querySelector(".content").innerHTML = products
-        .map(
-          (product) =>
-            `<div class="card" style="width: 18rem;">
+fetch("https://646e69389c677e23218ba227.mockapi.io/Products")
+  .then((result) => result.json())
+  .then((products) => {
+    document.querySelector(".content").innerHTML = products
+      .map(
+        (product) =>
+          `<div class="card" style="width: 18rem;">
               <img src="${product.productImage}" class="card-img-top" alt="...">
               <div class="card-body">
                   <h5 class="card-title">${product.productName}</h5>
@@ -16,19 +15,16 @@ document.addEventListener("DOMContentLoaded", function () {
                </div>
             </div>
         </div>`
-        )
-        .join("");
+      )
+      .join("");
 
-      // crearea paginii detail
+    // crearea paginii detail
 
-      let detailButtons = document.querySelectorAll(".detailButton");
-      console.log(detailButtons);
-      detailButtons.forEach((detailButton) => {
-        detailButton.addEventListener("click", function () {
-          const productId = detailButton.getAttribute("data-product-id");
-
-          console.log(`ai dat click pe produsul cu ID ${productId}`);
-        });
+    const detailButtons = document.querySelectorAll(".detailButton");
+    detailButtons.forEach((detailButton) => {
+      detailButton.addEventListener("click", function () {
+        const productId = detailButton.getAttribute("data-product-id");
+        window.open(`detail.html?id= ${productId} _blank`);
       });
     });
-});
+  });
