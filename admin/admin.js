3580f62808bd3f2edb822c69643ccbabe944967e
Mateas Petru <mateaspetru@gmail.com>
2023-06-04 09:18:16 +0300

    create async function for fetch data

--- admin/admin.js
+++ admin/admin.js
@@ -1,10 +1,10 @@
+// constant
 const url = `https://646e69389c677e23218ba227.mockapi.io/Products`;
+const addNewProduct = document.querySelector(".add-new-product");
+//
+fetchdata();
 
-fetch(url)
-  .then((result) => result.json())
-  .then((products) => {
-    populateWithProducts(products);
-  });
+// function
 
 function populateWithProducts(products) {
   document.querySelector("tbody").innerHTML = products
@@ -19,6 +19,7 @@ function populateWithProducts(products) {
             <button
               type="button"
               class="btn btn-danger remove-product-from-basket"
+              product-id="${product.id}"
             >
               Remove
             </button>
@@ -26,4 +27,13 @@ function populateWithProducts(products) {
         </tr>`
     )
     .join("");
+}
+async function fetchdata() {
+  try {
+    const response = await fetch(url);
+    const products = await response.json();
+    populateWithProducts(products);
+  } catch (error) {
+    console.log(error);
+  }
 }
