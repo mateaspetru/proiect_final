ffe613e2f1046cdfa5b881d14b06dc072c29f99f
Mateas Petru <mateaspetru@gmail.com>
2023-06-04 10:45:35 +0300

    create html structure when add new product button is press

--- admin/admin.js
+++ admin/admin.js
@@ -1,8 +1,10 @@
 // constant
 const url = `https://646e69389c677e23218ba227.mockapi.io/Products`;
 const addNewProduct = document.querySelector(".add-new-product");
+
 //
 fetchdata();
+addNewProduct.addEventListener("click", onClickCreateInputsForNewProduct);
 
 // function
 
@@ -35,5 +37,56 @@ async function fetchdata() {
     populateWithProducts(products);
   } catch (error) {
     console.log(error);
+  }
+}
+
+function onClickCreateInputsForNewProduct(e) {
+  if (e.target.classList.contains("add-new-product")) {
+    const inputsForNewProduct = document.querySelector(".container");
+    inputsForNewProduct.innerHTML = `
+    <div class="antet">
+        <div class="flex2">
+            <h1>Adaugare produs</h1>
+        </div>
+        <div class="flex1">
+            <button type="button" class="btn btn-primary width-50  save">
+              <i class="fa-solid fa-plus"></i>
+              Save
+            </button>
+            <button type="button" class="btn btn-primary width-50 btn-light cancel">
+            
+              Cancel
+            </button>
+        </div>
+    </div>
+    <div class="inputs">
+
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">Imagine</span>
+        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">Nume</span>
+        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+
+    <div class="input-group input-group-lg mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-lg">Descriere</span>
+        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+    </div>
+
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">Pret</span>
+        <input type="number" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">Cantitate</span>
+        <input type="number" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+
+    </div>
+    `;
   }
 }
