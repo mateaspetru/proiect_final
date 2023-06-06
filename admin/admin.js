6360ce073704fdd51a81908abc90476a9e875e8a
Mateas Petru <mateaspetru@gmail.com>
2023-06-06 11:04:20 +0300

    fully functional admin page

--- admin/admin.js
+++ admin/admin.js
@@ -1,16 +1,12 @@
-// constant
 const url = `https://646e69389c677e23218ba227.mockapi.io/Products`;
 const addNewProduct = document.querySelector(".add-new-product");
 const tBody = document.querySelector("tbody");
-// end of constants
 
-//call
 fetchdata();
 addNewProduct.addEventListener("click", addProduct);
 tBody.addEventListener("click", removeProductFromDataBase);
-// end of call
+tBody.addEventListener("click", editProduct);
 
-// function
 async function fetchdata() {
   try {
     const response = await fetch(url);
@@ -24,7 +20,7 @@ function populateWithProducts(products) {
       (product) =>
         `<tr>
           <td><img src="${product.image}" alt="" /></td>
-          <td class="product-name" product-id="${product.id}"><a href="">${product.name}</a> </td>
+          <td class="product-name" product-id="${product.id}"><a class="product-edit" product-id="${product.id}">${product.name}</a> </td>
           <td>${product.price}</td>
           <td>${product.stock}</td>
           <td>
@@ -109,7 +105,7 @@ function postProduct(product) {
     body: JSON.stringify(product),
   });
 }
-function htmlStructureNewProductAdd() {
+function htmlStructureNewProductAdd(product) {
   const inputsForNewProduct = document.querySelector(".container");
 
   inputsForNewProduct.innerHTML = `
@@ -118,7 +114,7 @@ function htmlStructureNewProductAdd() {
             <h1>Adaugare produs</h1>
         </div>
         <div class="flex1">
-            <button type="button" class="btn btn-primary width-50  save">
+            <button type="button" class="btn btn-primary width-50  save"  >
               <i class="fa-solid fa-plus"></i>
               Save
             </button>
@@ -158,5 +154,104 @@ function htmlStructureNewProductAdd() {
     </div>
     `;
 }
+async function editProduct(e) {
+  if (e.target.classList.contains("product-edit")) {
+    const productId = e.target.getAttribute("product-id");
+    try {
+      const response = await fetch(url + "/" + productId);
+      const product = await response.json();
+      htmlStructureNewProductEdit(product);
+      document.querySelector(".imagine-produs").value = product.image;
+      document.querySelector(".nume-produs").value = product.name;
+      document.querySelector(".descriere-produs").value = product.detail;
+      document.querySelector(".pret-produs").value = product.price;
+      document.querySelector(".cantitate-produs").value = product.stock;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  document.querySelector(".save").addEventListener("click", function (e) {
+    if (e.target.classList.contains("save")) {
+      e.stopPropagation();
+      const productId = document
+        .querySelector(".save")
+        .getAttribute("product-id");
+      const imagine = document.querySelector(".imagine-produs").value;
+      const nume = document.querySelector(".nume-produs").value;
+      const descriere = document.querySelector(".descriere-produs").value;
+      const pret = document.querySelector(".pret-produs").value;
+      const cantitate = document.querySelector(".cantitate-produs").value;
+      const product = {
+        name: nume,
+        price: pret,
+        image: imagine,
+        detail: descriere,
+        stock: cantitate,
+      };
+      fetch(url + "/" + productId, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(product),
+      }).then((response) => {
+        if (response.ok) {
+          location.reload();
+        }
+      });
+    }
+  });
+  document.querySelector(".cancel").addEventListener("click", function (e) {
+    e.stopPropagation;
+    location.reload();
+  });
+}
+function htmlStructureNewProductEdit(product) {
+  const inputsForNewProduct = document.querySelector(".container");
 
-// end of function
+  inputsForNewProduct.innerHTML = `
+    <div class="antet">
+        <div class="flex2">
+            <h1>Adaugare produs</h1>
+        </div>
+        <div class="flex1">
+            <button type="button" class="btn btn-primary width-50  save" product-id="${product.id}" >
+              <i class="fa-solid fa-plus"></i>
+              Save
+            </button>
+            <button type="button" class="btn btn-primary width-50 btn-light cancel">
+            
+              Cancel
+            </button>
+        </div>
+    </div>
+    <div class="inputs">
+
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">Imagine</span>
+        <input type="text" class="form-control imagine-produs" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">Nume</span>
+        <input type="text" class="form-control nume-produs" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+
+    <div class="input-group input-group-lg mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-lg">Descriere</span>
+        <input type="text" class="form-control descriere-produs" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+    </div>
+
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">Pret</span>
+        <input type="number" class="form-control pret-produs" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">Cantitate</span>
+        <input type="number" class="form-control cantitate-produs" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+
+    </div>
+    `;
+}
